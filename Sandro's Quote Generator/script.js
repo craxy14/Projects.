11b9cb1fd6129ebe395cde155f781ae8d60e8f97
craxy14 <xxmarselxx5@gmail.com>
2024-05-31 00:00:54 +0400

    second commit

---
+++ Sandro's Quote Generator/script.js
@@ -0,0 +1,16 @@
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const btn = document.getElementById("new")
+
+const api_url = "https://api.quotable.io/random"
+
+
+async function getquote(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    quote.innerHTML = data.content
+    author.innerHTML = data.author
+}
+
+
+getquote(api_url)
